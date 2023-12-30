d9c2720839d8d8c812cca1e437a976e4f51b7c96
Azamat Rasulov <azamat.rasulov00@gmail.com>
2023-12-30 16:15:02 +0900

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Azamat</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop dynamic web applications
+            I develop dynamic web applications &nbsp;
             <br className="sm:block hidden" />
             using MERN stack technologies.
           </p>
