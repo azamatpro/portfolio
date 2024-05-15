bbb7c6dd93dcb4af83e561b36e502b149f6a8d4f
azamatpro <azamat.rasulov00@gmail.com>
2024-05-15 17:25:12 +0900

    Testmonials updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -150,8 +150,7 @@ const experiences = [
 
 const testimonials = [
   {
-    testimonial:
-      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+    testimonial: 'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
     name: 'Sara Lee',
     designation: 'CFO',
     company: 'Acme Co',
@@ -159,15 +158,14 @@ const testimonials = [
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: 'Chris Brown',
-    designation: 'COO',
-    company: 'DEF Corp',
-    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+      'I have known Azamat for two years and can attest to his strengths and quality of character. He distinguished himself by working exceptionally hard in my course: Artificial intelligence.',
+    name: 'Vijay Kakani',
+    designation: 'Assistant Professor',
+    company: 'Inha University',
+    image: professor,
   },
   {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    testimonial: "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
     name: 'Lisa Wang',
     designation: 'CTO',
     company: '456 Enterprises',
