f10cd714f3b994cd7e63ab472490e5d3e6066721
Azamat Rasulov <azamat.rasulov00@gmail.com>
2024-05-15 16:13:10 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
   Tech,
   Works,
   StarsCanvas,
+  Feedbacks,
 } from './components';
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
+        <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
