980415129aed903be0d23befe7d886cfe60820d6
azamatpro <azamat.rasulov00@gmail.com>
2024-11-04 10:17:24 +0900

    env vars update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -77,7 +77,7 @@ const Contact = () => {
     if (isEmailSent.message) {
       const timer = setTimeout(() => {
         setIsEmailSent({ type: '', message: '' });
-      }, 5000);
+      }, 6000);
 
       // Cleanup timer on component unmount or when message changes
       return () => clearTimeout(timer);
@@ -89,6 +89,7 @@ const Contact = () => {
       <motion.div variants={slideIn('left', 'tween', 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
+
         <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Name</span>
@@ -96,6 +97,7 @@ const Contact = () => {
               type='text'
               name='name'
               value={form.name}
+              required
               onChange={handleChange}
               placeholder="What's your good name?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
@@ -106,24 +108,14 @@ const Contact = () => {
             <input
               type='email'
               name='email'
+              required
               value={form.email}
               onChange={handleChange}
               placeholder="What's your email address?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea
-              rows={7}
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-
+          {/* Conditionally render the message container */}
           {isEmailSent.message && (
             <div
               className={`p-4 rounded-md text-white ${isEmailSent.type === 'success' ? 'bg-green-500' : 'bg-red-500'}`}
@@ -131,10 +123,22 @@ const Contact = () => {
               {isEmailSent.message}
             </div>
           )}
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows={7}
+              name='message'
+              value={form.message}
+              required
+              onChange={handleChange}
+              placeholder='What you want to say?'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
 
           <button
             type='submit'
-            disabled={!form.email}
+            disabled={!form.message}
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
             {loading ? 'Sending...' : 'Send'}
